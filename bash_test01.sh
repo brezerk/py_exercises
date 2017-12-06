5f255a6403e022a8a3121224e61ab265df9cf89e
Oleksii S. Malakhov <brezerk@gmail.com>
2017-12-06 14:36:14 +0200

    Get rid of utf8 chars

--- bash_test01.sh
+++ bash_test01.sh
@@ -2,7 +2,7 @@
 
 #
 # Bash script test 00:
-# Search for “my konfu is the best” in all *.py files, print matched file list
+# Search for "my konfu is the best" in all *.py files, print matched file list
 #
 
 find -type f -name '*.py' -exec grep 'my konfu is the best' /dev/null {} \;
